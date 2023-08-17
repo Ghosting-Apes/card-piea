318721f2a291d7e483f4d053159fb5cacd5dcc20
Henock Dinberu <henockdinberu11@gmail.com>
2023-08-17 03:38:43 -0400

    redgomugomu + texture/sprite code

--- Cards/BaseCard.cs
+++ Cards/BaseCard.cs
@@ -1,12 +1,17 @@
 using Godot;
 using System;
 
-public partial class Sprite2D : Godot.Sprite2D
+public partial class BaseCard : MarginContainer
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-	}
+        var texture = (Texture2D)GD.Load("res://Images/redgomugomu10.png");
+        var sprite = GetNode<Sprite2D>("CardImg");
+        sprite.Texture = texture;
+
+        GD.Print("WORKS WITH NEW CLASS");
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
