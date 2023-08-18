292c5c19cf5bfabc8d1d3e827a6321e9c741ac96
AtwanZ <33740557+AtwanZ@users.noreply.github.com>
2023-08-18 01:03:16 -0400

    Added more cards

--- Cards/BaseCard.cs
+++ Cards/BaseCard.cs
@@ -7,21 +7,49 @@ public partial class BaseCard : MarginContainer
 	public override void _Ready()
 	{
 		Random random = new Random();
-		var x = random.Next(0, 2);
+		var x = random.Next(0, 6);
 
 		Texture2D texture;
-        if (x == 0)
+		
+		
+		switch(x) 
 		{
-            texture = (Texture2D)GD.Load("res://Images/redgomugomu10.png");
-        }
+			case 0: texture = (Texture2D)GD.Load("res://Images/redgomugomu10.png");
+			break;
+			
+			case 1: texture = (Texture2D)GD.Load("res://Images/orangegomugomu10.png");
+			break;
+			
+			case 2: texture = (Texture2D)GD.Load("res://Images/yellowgomugomu10.png");
+			break;
+			
+			case 3: texture = (Texture2D)GD.Load("res://Images/greengomugomu10.png");
+			break;
+			
+			case 4: texture = (Texture2D)GD.Load("res://Images/bluegomugomu10.png");
+			break;
+			
+			case 5: texture = (Texture2D)GD.Load("res://Images/purplegomugomu10.png");
+			break;
+			
+			default: texture = (Texture2D)GD.Load("res://Images/blankcard.png");
+			break;
+		}
+		
+		/*
+		if (x == 0)
+		{
+			texture = (Texture2D)GD.Load("res://Images/redgomugomu10.png");
+		}
 		else
 		{
-            texture = (Texture2D)GD.Load("res://Images/purplegomugomu10.png");
-        }
+			texture = (Texture2D)GD.Load("res://Images/purplegomugomu10.png");
+		}
+		*/
 		
 		var sprite = GetNode<Sprite2D>("CardImg");
-        sprite.Texture = texture;
-    }
+		sprite.Texture = texture;
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
--- Playspace.cs
+++ Playspace.cs
@@ -20,21 +20,21 @@ public partial class Playspace : Node2D
 			newCard.Name = $"Card {i} for Player 1";
 			int x = (100 * i) + 75;
 			int y = 500;
-            newCard.Position = new Vector2(x, y);
+			newCard.Position = new Vector2(x, y);
 			playerOneDeck.AddChild(newCard);
 		}
 
 		// Player 2 Deck
-        for (int i = 0; i < 5; i++)
-        {
-            var newCard = (MarginContainer)BaseCard.Instantiate();
-            newCard.Name = $"Card {i} for Player 2";
+		for (int i = 0; i < 5; i++)
+		{
+			var newCard = (MarginContainer)BaseCard.Instantiate();
+			newCard.Name = $"Card {i} for Player 2";
 			int x = (100 * i) + 650;
 			int y = 100;
-            newCard.Position = new Vector2(x, y);
-            playerTwoDeck.AddChild(newCard);
-        }
-    }
+			newCard.Position = new Vector2(x, y);
+			playerTwoDeck.AddChild(newCard);
+		}
+	}
 
 	public override void _UnhandledInput(InputEvent @event)
 	{
@@ -43,15 +43,15 @@ public partial class Playspace : Node2D
 			if (clicked.Pressed)
 			{
 				GD.Print($"CLICKED ON SCREEN {GetGlobalMousePosition()}");
-                /*				var new_card = (MarginContainer)BaseCard.Instantiate();
-                                new_card.Name = "CARD";
-                                new_card.Position = GetGlobalMousePosition();
+				/*				var new_card = (MarginContainer)BaseCard.Instantiate();
+								new_card.Name = "CARD";
+								new_card.Position = GetGlobalMousePosition();
 
-                                var deck = GetNode<Node>("Deck");
-                                deck.AddChild(new_card);
-                                base._UnhandledInput(@event);*/
-            }
-        }
+								var deck = GetNode<Node>("Deck");
+								deck.AddChild(new_card);
+								base._UnhandledInput(@event);*/
+			}
+		}
 
 	}
 
